2ad0e7e728d5b08de4cf0245ed4786d3ee7b8260
Yashish Hirani <yh3414@nyu.edu>
2023-11-14 18:36:59 -0500

    Simple List Added with Clicking Features

--- game_library/src/components/ListGroup.tsx
+++ game_library/src/components/ListGroup.tsx
@@ -1,18 +1,26 @@
+import { useState } from "react";
+
 function ListGroup() {
   let items = ["New York", "San Francisco", "Miami", "Chicago", "Seattle"];
-  items = [];
-
-  const getMessage=() => {
-    return items.length === 0 ? <p>There are no items!</p> : null;;
-  }
+  const [selecedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>List Group</h1>
-      {getMessage()}
+      {items.length === 0 && <p>There are no items!</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selecedIndex == index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {setSelectedIndex(index)}}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
